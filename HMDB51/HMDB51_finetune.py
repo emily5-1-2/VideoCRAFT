336a71373a34f45894a8fece1d8984b841c00585
emily5-1-2 <emilygnzsas5@gmail.com>
2024-06-25 16:26:25 -0700

    Separated code into files

--- HMDB51/HMDB51_finetune.py
+++ HMDB51/HMDB51_finetune.py
@@ -1,12 +1,10 @@
 import torch
-from torch.utils.data import random_split, DataLoader
 from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
-import torchvision
-import transforms as T
 import os
 
 from HMDB51_model import VideoRecog_Model
 from HMDB51_training_helper import train, test
+from HMDB51_dataloaders import get_dataloaders
 
 import argparse
 
@@ -21,6 +19,8 @@ parser.add_argument('--val_split', type=float, default=0.05)
 parser.add_argument('--num_frames', type=int, default=16)
 parser.add_argument('--clip_steps', type=int, default=50)
 parser.add_argument('--crop_size', type=int, default=112)
+parser.add_argument('--video_dir', type=str, default='hmdb51_video_data/')
+parser.add_argument('--split_dir', type=str, default='hmdb51_test_train_splits')
 
 #TRAINING PARAMS
 parser.add_argument('--num_workers', type=int, default=8)
@@ -35,38 +35,7 @@ parser.add_argument('--lr_factor', type=float, default=0.5)
 parser.add_argument('--min_lr', type=float, default=1e-8)
 
 hmdb_args = parser.parse_args()
-    
-train_tfms = torchvision.transforms.Compose([
-                                 T.ToFloatTensorInZeroOne(),
-                                 T.Resize((128, 171)),
-                                 T.RandomHorizontalFlip(),
-                                 T.Normalize(mean=[0.43216, 0.394666, 0.37645], std=[0.22803, 0.22145, 0.216989]),
-                                 T.RandomCrop((hmdb_args.crop_size, hmdb_args.crop_size))
-                               ]) 
 
-test_tfms =  torchvision.transforms.Compose([
-                                             T.ToFloatTensorInZeroOne(),
-                                             T.Resize((128, 171)),
-                                             T.Normalize(mean=[0.43216, 0.394666, 0.37645], std=[0.22803, 0.22145, 0.216989]),
-                                             T.CenterCrop((hmdb_args.crop_size, hmdb_args.crop_size))
-                                             ])
-
-hmdb51_train = torchvision.datasets.HMDB51('hmdb51_video_data/', 'hmdb51_test_train_splits/', hmdb_args.num_frames,
-                                                step_between_clips = hmdb_args.clip_steps, fold=1, train=True,
-                                                transform=train_tfms, num_workers=hmdb_args.num_workers)
-
-hmdb51_test = torchvision.datasets.HMDB51('hmdb51_video_data/', 'hmdb51_test_train_splits/', hmdb_args.num_frames,
-                                                step_between_clips = hmdb_args.clip_steps, fold=1, train=False,
-                                                transform=test_tfms, num_workers=hmdb_args.num_workers)
-      
-total_train_samples = len(hmdb51_train)
-total_val_samples = round(hmdb_args.val_split * total_train_samples)
-
-print(f"number of train samples {total_train_samples}")
-print(f"number of validation samples {total_val_samples}")
-print(f"number of test samples {len(hmdb51_test)}")
-
-bs = hmdb_args.batch_size
 lr = hmdb_args.lr
 gamma = hmdb_args.gamma
 lr_red_step = hmdb_args.lr_red_step
@@ -77,14 +46,7 @@ patience = hmdb_args.patience
 lr_factor = hmdb_args.lr_factor
 min_lr = hmdb_args.min_lr
 
-kwargs = {'num_workers':num_workers, 'pin_memory':True} if torch.cuda.is_available() else {'num_workers':num_workers}
-
-hmdb51_train_v1, hmdb51_val_v1 = random_split(hmdb51_train, [total_train_samples - total_val_samples,
-                                                                       total_val_samples])
-  
-train_loader = DataLoader(hmdb51_train_v1, batch_size=bs, shuffle=True, **kwargs)
-val_loader   = DataLoader(hmdb51_val_v1, batch_size=bs, shuffle=True, **kwargs)
-test_loader  = DataLoader(hmdb51_test, batch_size=bs, shuffle=False, **kwargs)
+train_loader, val_loader, test_loader = get_dataloaders(hmdb_args)
 
 model = VideoRecog_Model(hmdb_args)
 
