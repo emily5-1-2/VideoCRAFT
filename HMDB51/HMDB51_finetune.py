b69ba0d31a1f3d598893f83193f3f4e3517b1c70
Emily Gu <exgu@caltech.edu>
2024-06-24 17:39:15 -0700

    Change lr_scheduler

--- HMDB51/HMDB51_finetune.py
+++ HMDB51/HMDB51_finetune.py
@@ -16,16 +16,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--dropout', type=float, default=0.3)
 parser.add_argument('--fix_base', type=bool, default=False)
 
-#TRAINING PARAMS
+#DATASET PARAMS
 parser.add_argument('--val_split', type=float, default=0.05)
 parser.add_argument('--num_frames', type=int, default=16)
 parser.add_argument('--clip_steps', type=int, default=50)
+parser.add_argument('--crop_size', type=int, default=112)
+
+#TRAINING PARAMS
 parser.add_argument('--num_workers', type=int, default=8)
 parser.add_argument('--batch_size', type=int, default=4)
 parser.add_argument('--lr', type=float, default=5e-2)
-parser.add_argument('--gamma', type=float, default=0.7)
+parser.add_argument('--gamma', type=float, default=0.9)
+parser.add_argument('--lr_red_step', type=int, default=3)
 parser.add_argument('--total_epochs', type=int, default=10)
 parser.add_argument('--ckpt_freq', type=int, default=5)
+parser.add_argument('--patience', type=int, default=2)
+parser.add_argument('--lr_factor', type=float, default=0.5)
+parser.add_argument('--min_lr', type=float, default=1e-8)
 
 hmdb_args = parser.parse_args()
     
@@ -34,14 +41,14 @@ train_tfms = torchvision.transforms.Compose([
                                  T.Resize((128, 171)),
                                  T.RandomHorizontalFlip(),
                                  T.Normalize(mean=[0.43216, 0.394666, 0.37645], std=[0.22803, 0.22145, 0.216989]),
-                                 T.RandomCrop((112, 112))
+                                 T.RandomCrop((hmdb_args.crop_size, hmdb_args.crop_size))
                                ]) 
 
 test_tfms =  torchvision.transforms.Compose([
                                              T.ToFloatTensorInZeroOne(),
                                              T.Resize((128, 171)),
                                              T.Normalize(mean=[0.43216, 0.394666, 0.37645], std=[0.22803, 0.22145, 0.216989]),
-                                             T.CenterCrop((112, 112))
+                                             T.CenterCrop((hmdb_args.crop_size, hmdb_args.crop_size))
                                              ])
 
 hmdb51_train = torchvision.datasets.HMDB51('hmdb51_video_data/', 'hmdb51_test_train_splits/', hmdb_args.num_frames,
@@ -62,9 +69,13 @@ print(f"number of test samples {len(hmdb51_test)}")
 bs = hmdb_args.batch_size
 lr = hmdb_args.lr
 gamma = hmdb_args.gamma
+lr_red_step = hmdb_args.lr_red_step
 total_epochs = hmdb_args.total_epochs
 config = {}
 num_workers = hmdb_args.num_workers
+patience = hmdb_args.patience
+lr_factor = hmdb_args.lr_factor
+min_lr = hmdb_args.min_lr
 
 kwargs = {'num_workers':num_workers, 'pin_memory':True} if torch.cuda.is_available() else {'num_workers':num_workers}
 
@@ -91,8 +102,8 @@ if torch.cuda.is_available():
 
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-#scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
-scheduler = ReduceLROnPlateau(optimizer=optimizer, patience=3, factor=0.5, min_lr=1e-8)
+scheduler = StepLR(optimizer, step_size=lr_red_step, gamma=gamma)
+#scheduler = ReduceLROnPlateau(optimizer=optimizer, patience=patience, factor=lr_factor, min_lr=min_lr)
 
 print("Launching Action Recognition Model training")
 os.makedirs('ckpts', exist_ok=True)
@@ -100,10 +111,10 @@ for epoch in range(1, total_epochs + 1):
     train(config, model, train_loader, optimizer, epoch)
     val_loss = test(model, val_loader, text="Validation")
     if (epoch+1) % hmdb_args.ckpt_freq == 0:
-        ckpt_path = os.path.join('ckpts', f'ViT-{epoch+1}epochs.pt')
+        ckpt_path = os.path.join('ckpts', f'HMDB51-{epoch+1}epochs.pt')
         torch.save(model.state_dict(), ckpt_path)
         print(f'Checkpoint saved at {ckpt_path}')
-    scheduler.step(val_loss)
+    scheduler.step()
 
 test(model, test_loader, text="Test")
 
