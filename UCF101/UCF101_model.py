db9d283c96afde859b0293c407b3ecfde0a0385d
Emily Gu <exgu@caltech.edu>
2024-06-27 15:02:09 -0700

    Fixed dropout

--- UCF101/UCF101_model.py
+++ UCF101/UCF101_model.py
@@ -7,7 +7,7 @@ class VideoRecog_Model(nn.Module):
       super(VideoRecog_Model, self).__init__()
       self.base_model = nn.Sequential(*list(r3d_18(weights=True).children())[:-1])
       self.fc1 = nn.Linear(512, 101)
-      self.dropout = nn.Dropout2d(args.dropout)
+      self.dropout = nn.Dropout(args.dropout)
 
   def forward(self, x):
       out = self.base_model(x).squeeze(4).squeeze(3).squeeze(2) # output of base model is bs x 512 x 1 x 1 x 1
